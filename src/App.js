e9a22f3a515567294d3c543d8312017e89f66844
rui <p111553xt@gmail.com>
2022-12-09 17:54:02 +0800

    react page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
-function App() {
-  return <div className="App"></div>;
-}
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>MY Profile</h1>
+      <p>My name is rui.</p>
+      <p>I am 25 years old.</p>
+    </div>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,12 @@
-// const React = require("react");
-// const ReactDOM = require("react-dom");
-
-// es6
 import React from "react";
 import ReactDOM from "react-dom";
-// function App() {
-//   return React.createElement("div", null, [
-//     React.createElement("h1", { style: { color: "red" } }, "My Profile"),
-//     React.createElement("p", null, "My name is rui lee."),
-//     React.createElement("button", null, "Check my profile."),
-//   ]);
-// }
+import App from "./App";
+import Nav from "./Nav";
 
-// jsx
-function App() {
-  return (
-    <div>
-      <h1>My Profile</h1>
-      <p>My name is rui lee.</p>
-      <button>Check my profile.</button>
-    </div>
-  );
-}
-
-ReactDOM.render(App(), document.querySelector("#root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <Nav />
+    <App />
+  </React.StrictMode>,
+  document.querySelector("#root")
+);
