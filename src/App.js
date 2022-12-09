f35db58c6fd61ac86598bff82467006d0124032d
rui <p111553xt@gmail.com>
2022-12-09 18:27:25 +0800

    css in jsx

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <h1>MY Profile</h1>
+      <h1 style={{ color: "red", fontSize: "10rem" }}>MY Profile</h1>
       <p>My name is rui.</p>
       <p>I am 25 years old.</p>
     </div>
