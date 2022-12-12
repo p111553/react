45bb7493e74d0c29beacca1b6dc221ac3255f5dd
rui <p111553xt@gmail.com>
2022-12-12 19:12:33 +0800

    state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 import React, { useState } from "react";
 
 const App = () => {
-  let [name, setName] = useState("Rui Lee");
-  const changeNameHandler = () => {
-    setName("mike huang");
-  };
-  return (
-    <div>
-      <h1>{name}</h1>
-      <button onClink={changeNameHandler}>change Name</button>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
